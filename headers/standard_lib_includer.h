9017b9a7ce3a9ecc66cff961093ae624668b8e41
fire_wizard <slaventiysvat@gmail.com>
2024-05-21 14:33:28 +0300

    modified libraryes changes structure of projects

--- headers/standard_lib_includer.h
+++
@@ -1,8 +0,0 @@
-#ifndef STANDART_LIB_INCLUDER
-#define STANDART_LIB_INCLUDER
-
-#include <vector>
-#include <iostream>
-#include "custom_types.h"
-
-#endif /*STANDART_LIB_INCLUDER*/
---
+++ image_processor/inc/includes.hpp
@@ -0,0 +1,24 @@
+#pragma once 
+#include "custom_types.h"
+#include "standard_lib_includer.h"
+#include <fmt/core.h>
+#include "opencv2/imgproc.hpp"
+#include "opencv2/imgcodecs.hpp"
+#include "opencv2/highgui.hpp"
+
+//#include <iostream>
+//#include <string>
+#include "image_estimator.h"
+#include "image_noiser.h"
+#include "stream_manager.h"
+#include "simple_image_filters.h"
+//#include "stream_manager.h"
+#include <opencv2/photo.hpp>
+#include <cmath>
+//
+#include "convolution_tools.h"
+#include "gauss_filter.h"
+#include "median_filter.h"
+#include "nlm_filter.h"
+#include "filters.h"
+#include "image_mat_tools.h"
