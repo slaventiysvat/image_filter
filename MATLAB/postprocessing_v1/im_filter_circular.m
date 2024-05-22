46f19320a5dc6f6c934baaa335b7186de7b5a0a1
fire_wizard <slaventiysvat@gmail.com>
2024-05-23 01:10:54 +0300

    added docs and comments to reference algorithm

--- MATLAB/postprocessing_v1/im_filter_circular.m
+++ MATLAB/postprocessing_v1/im_filter_circular.m
@@ -1,16 +1,17 @@
 function  out_image = im_filter_circular(input_image,mask)
-%works only for RGB image
-rgb_R = input_image(:,:,1);
-rgb_G = input_image(:,:,2);
-rgb_B = input_image(:,:,3);
 
-%of image will be replaced using circular approach
-f_R=imfilter(rgb_R,mask,'circular');
-f_G=imfilter(rgb_G,mask,'circular');
-f_B=imfilter(rgb_B,mask,'circular');
+    %works only for RGB image
+    rgb_R = input_image(:,:,1);
+    rgb_G = input_image(:,:,2);
+    rgb_B = input_image(:,:,3);
+
+    %of image will be replaced using circular approach
+    f_R=imfilter(rgb_R,mask,'circular');
+    f_G=imfilter(rgb_G,mask,'circular');
+    f_B=imfilter(rgb_B,mask,'circular');
         
-%getting new image (filtred)
-rgb_tmp = cat(3,f_R,f_G,f_B);        
-out_image = imsubtract(input_image,rgb_tmp);
+    %getting new image (filtred)
+    rgb_tmp = cat(3,f_R,f_G,f_B);        
+    out_image = imsubtract(input_image,rgb_tmp);
 
 end
--- MATLAB/postprocessing_v1/image_postprocessing_v1.m
+++ MATLAB/postprocessing_v1/image_postprocessing_v1.m
@@ -2,36 +2,43 @@ clear all
 close all
 clc
 
-% r = -12:1:10;
-% max_r = max(r);
-
+%Load compressed image, normilize it
 image_in = imread('artificial.bmp');
 image_in = double(image_in);
 image_in = image_in./255;
 
+%Load original image, normilize it
 image_in_org = imread('DeltaE_8bit_gamma2.2.bmp');
 image_in_org = double(image_in_org);
 image_in_org = image_in_org./255;
 
+%add Gaussian Noise to image
 J = image_in;
 J = imnoise(image_in,'gaussian',0.02);
 figure(1)
 imshow(J)
 title('Image with Noise')
+
+%make Grayscale image and add gaus noise
 image_gray = rgb2gray(J);
 B = imgaussfilt(J,2.5);
 %B = J;
 % figure(2)
 % imshow(B)
 % title('Image after Gauss Filter')
+
 [m n k] = size(image_in);
 image_out = zeros(m,n,k);
 sigma = 0.1;
 alpha = 0.5;
+
+%use median filter to improving image
 % image_out = locallapfilt(single(B), sigma, alpha);%returning contrasts!!!
 image_out(:,:,1) = medfilt2(B(:,:,1),[5 5]);
 image_out(:,:,2) = medfilt2(B(:,:,2),[5 5]);
 image_out(:,:,3) = medfilt2(B(:,:,3),[5 5]);
+
+%measure sigma from cropped part of image
 noisyLAB = rgb2lab(B);
 roi = [210,24,52,41];
 patch = imcrop(noisyLAB,roi);
@@ -40,13 +47,12 @@ patchSq = patch.^2;
 edist = sqrt(sum(patchSq,3));
 patchSigma = sqrt(var(edist(:)));
 
+%use nlm filter to filtering image
 DoS = 0.25*patchSigma;
 image_out = imnlmfilt(B,'DegreeOfSmoothing',DoS);
 figure(3)
 imshow(image_out)
 title('Image after NLM filter')
-
-
 
 lapMatrix=[1 1 1;
             1 -8 1;
@@ -54,30 +60,32 @@ lapMatrix=[1 1 1;
  Matrix2=[1 0 1;
     0 -4 0;
     1 0 1];
+
+%use median filter with circular filter 
 rgb_sharped = im_filter_circular(image_out,lapMatrix);
 r = rgb_sharped(:,:,1)./max((max(rgb_sharped(:,:,1))));
 g = rgb_sharped(:,:,1)./max((max(rgb_sharped(:,:,1))));
 b = rgb_sharped(:,:,1)./max((max(rgb_sharped(:,:,1))));
+
 r1 = uint8((r.*255));
 J = im2uint8(r);
-% rgb_sharped1 = im_filter_circular(image_out,Matrix2);
+
+%use circular filter for filtering image
+rgb_sharped1 = im_filter_circular(image_out,Matrix2);
 
 figure(1);
-% subplot(311);
+subplot(221);
 imshow(image_in);
 title('Input Image');
-% subplot(312);
-figure(2);
+subplot(222);
 imshow(image_out);
 title('Image Out "Blurred"');
-% subplot(313)
-figure(3);
+subplot(223)
 imshow(rgb_sharped);
 title('Image Out "Sharped1"');
-
-% figure(4);
-% imshow(rgb_sharped1);
-% title('Image Out "Sharped2"');
+subplot(224)
+imshow(rgb_sharped1);
+title('Image Out "Sharped2"');
 
 %estimating PSNR and SSIM
 fprintf('===============SSIM Estimating================= \n');
