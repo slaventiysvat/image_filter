af22ed704b948e2d9dee48864cb3174685926e50
fire_wizard <slaventiysvat@gmail.com>
2024-06-05 00:53:33 +0300

    Prepearing before profiling

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 ﻿#include "includes.hpp"
 #include <sqlite3.h>
-
+#include <chrono>
+//#include <windows.h>
+#include <filesystem>
 std::string type2str(int type);
 Matrix<uint8_t> copy_one_m_channel2vector(cv::Mat image, int num_of_channel);
 
@@ -9,6 +11,13 @@ cv::Mat split3channels2image(Matrix<uint8_t> image_ch1,
     Matrix<uint8_t> image_ch3);
 
 using namespace cv;
+
+//std::wstring ExePath() {
+//	TCHAR buffer[MAX_PATH] = { 0 };
+//	GetModuleFileName(NULL, buffer, MAX_PATH);
+//	std::wstring::size_type pos = std::wstring(buffer).find_last_of(L"\\/");
+//	return std::wstring(buffer).substr(0, pos);
+//}
 
 //inline uchar Clamp(int n)
 //{
@@ -66,60 +75,169 @@ bool AddGaussianNoise_Opencv(const Mat mSrc, Mat& mDst, double Mean = 0.0, doubl
 
 int main(int argc, char** argv)
 {
+	//std::cout << "my directory is " << ExePath() << "\n";
 
-    std::string path1 = "../../../img/artifical_341_512.bmp";
-    std::string path2 = "../../../img/DeltaE_8bit_gamma2.2.bmp";
-	cv::Mat image_to_be_filtered, input_image_two;
-	cv::Mat im_after_gauss_noise;
-	cv::Mat im_after_gauss_filter;
-	cv::Mat im_after_median_filter;
-	cv::Mat im_after_nlm_filter;
-	cv::Mat im_after_laplace_filter;
+	//std::filesystem::path cwd = std::filesystem::current_path();
+	//std::cout << cwd << std::endl;
+ //   std::string path1 = "../../../../img/artifical_341_512.bmp";
+ //   std::string path2 = "../../../../img/DeltaE_8bit_gamma2.2.bmp";
+	//cv::Mat image_to_be_filtered, input_image_two;
+	//cv::Mat im_after_gauss_noise;
+	//cv::Mat im_after_gauss_filter;
+	//cv::Mat im_after_median_filter;
+	//cv::Mat im_after_nlm_filter;
+	//cv::Mat im_after_laplace_filter;
 
-	image_to_be_filtered = cv::imread(path1);
+	//image_to_be_filtered = cv::imread(path1);
 
-	cv::Mat image_artifacted = image_to_be_filtered;
-	 
-	input_image_two = cv::imread(path2);
+	//cv::Mat image_artifacted = image_to_be_filtered;
+	// 
+	//input_image_two = cv::imread(path2);
 
-	//add gaussian noise to image
-	image_noiser noiser_image;
-	simple_image_filters image_filter;
+	//imshow("Image Task", image_to_be_filtered);
+	//imshow("Image Original", input_image_two);
 
-	const float mean = 5.0;
-	const float sigma = 8.0;
+	//cv::waitKey(0);
 
-	//noiser_image.AddGaussianNoise_Opencv(image_to_be_filtered, im_after_gauss_noise, mean, sigma);
-    AddGaussianNoise_Opencv(image_to_be_filtered, im_after_gauss_noise, mean, sigma);
-	//split image on 3 channels
+		//imread test image
+		//std::string path1 = "./Images/artifical_341_512.bmp";
+		std::string path1 = "./../../../img/artificial.bmp";
+		std::string path2 = "./../../../img/DeltaE_8bit_gamma2.2.bmp";
+		cv::Mat blue_channe_one, input_image_two;
+		cv::Mat im_after_gauss_noise;
+		cv::Mat im_after_gauss_filter;
+		cv::Mat im_after_median_filter;
+		cv::Mat im_after_nlm_filter;
+		cv::Mat im_after_laplace_filter;
 
-	//smooth image with gauss filter
-    GaussianBlur(im_after_gauss_noise, im_after_gauss_noise, Size(3, 3), 0);
-	//im_after_gauss_filter = split3channels2image(red_channel, green_channel, blue_channel);
+		blue_channe_one = cv::imread(path1);
+		input_image_two = cv::imread(path2);
+		int wight = blue_channe_one.size().width;
+		int hight = blue_channe_one.size().height;
 
-	//apply median filter to image
-	//median_filter median_filter_tool;
-    medianBlur(im_after_gauss_noise, im_after_median_filter,5);
+		//need optimize now this code can works only with small images
+		cv::resize(blue_channe_one, blue_channe_one, cv::Size(wight, hight));
+		cv::resize(input_image_two, input_image_two, cv::Size(wight, hight));
 
-	////apply NLM filter to image
-	//nlm_filter nlm_filter_tool;
-    bilateralFilter(im_after_median_filter, im_after_nlm_filter, 1, 2, 0.5);
-	//im_after_nlm_filter = split3channels2image(red_channel, green_channel, blue_channel);
+		//add gaussian noise to image
+		image_noiser noiser_image;
+		simple_image_filters image_filter;
 
-	//apply laplase filter to image
+		const float mean = 5.0;
+		const float sigma = 8.0;
 
-	/*Matrix<float> kernel = { {1.0f,1.0f,1.0f},{1.0f,-8.0,1.0f},{1.0f,1.0f,1.0f} };*/
+		noiser_image.AddGaussianNoise_Opencv(blue_channe_one, im_after_gauss_noise, mean, sigma);
 
-	//Matrix<float> kernel = { {0.0f,0.0f,1.0f,0.0f,0.0f},{0.0f,1.0f,2.0f,1.0f,0.0f},{1.0f,2.0f,-16.0f,2.0f,1.0f},{0.0f,1.0f,2.0f,1.0f,0.0f},{0.0f,0.0f,1.0f,0.0f,0.0f}};
+		//split image on 3 channels
+		Matrix<uint8_t> red_channel = copy_one_m_channel2vector(im_after_gauss_noise, 0);
+		Matrix<uint8_t> green_channel = copy_one_m_channel2vector(im_after_gauss_noise, 1);
+		Matrix<uint8_t> blue_channel = copy_one_m_channel2vector(im_after_gauss_noise, 2);
 
-	//getting borders from original image
+		//smooth image with gauss filter
+		gauss_filter gauss_filt_tool;
+		red_channel = gauss_filt_tool.gauss_filter_2_dims(red_channel, 0.8);
+		green_channel = gauss_filt_tool.gauss_filter_2_dims(green_channel, 0.8);
+		blue_channel = gauss_filt_tool.gauss_filter_2_dims(blue_channel, 0.8);
 
-	/*im_after_laplace_filter = split3channels2image(red_channel, green_channel, blue_channel);*/
+		im_after_gauss_filter = split3channels2image(red_channel, green_channel, blue_channel);
 
-	imshow("Image Task", image_to_be_filtered);
-	imshow("Image Filtered", im_after_nlm_filter);
+		//apply median filter to image
+		median_filter median_filter_tool;
+		red_channel = median_filter_tool.median_filter_2_dims(red_channel, 3);
+		green_channel = median_filter_tool.median_filter_2_dims(green_channel, 3);
+		blue_channel = median_filter_tool.median_filter_2_dims(blue_channel, 3);
 
-	cv::waitKey(0);
+		im_after_median_filter = split3channels2image(red_channel, green_channel, blue_channel);
+
+		////apply NLM filter to image
+		/*nlm_filter nlm_filter_tool;
+		red_channel = nlm_filter_tool.nlm_filter_2_dims(red_channel, 5, 10, 0.04);
+		green_channel = nlm_filter_tool.nlm_filter_2_dims(green_channel, 5, 10, 0.04);
+		blue_channel = nlm_filter_tool.nlm_filter_2_dims(blue_channel, 5, 10, 0.04);*/
+
+		im_after_nlm_filter = split3channels2image(red_channel, green_channel, blue_channel);
+
+		//apply laplase filter to image
+
+		Matrix<float> kernel = { {1.0f,1.0f,1.0f},{1.0f,-8.0,1.0f},{1.0f,1.0f,1.0f} };
+
+		//Matrix<float> kernel = { {0.0f,0.0f,1.0f,0.0f,0.0f},{0.0f,1.0f,2.0f,1.0f,0.0f},{1.0f,2.0f,-16.0f,2.0f,1.0f},{0.0f,1.0f,2.0f,1.0f,0.0f},{0.0f,0.0f,1.0f,0.0f,0.0f}};
+
+		//getting borders from original image
+		Matrix<uint8_t> red_channel_orig = copy_one_m_channel2vector(blue_channe_one, 0);
+		Matrix<uint8_t> green_channel_orig = copy_one_m_channel2vector(blue_channe_one, 1);
+		Matrix<uint8_t> blue_channel_orig = copy_one_m_channel2vector(blue_channe_one, 2);
+
+		//Matrix<uint8_t> red_channel_filt = image_filter.simple_filter_2_dims(red_channel_orig, kernel);
+		//Matrix<uint8_t> green_channel_filt = image_filter.simple_filter_2_dims(green_channel_orig, kernel);
+		//Matrix<uint8_t> blue_channel_filt = image_filter.simple_filter_2_dims(blue_channel_orig, kernel);
+
+		filtersmy simple_filter;
+		image_mat_tools mat_tool;
+		Matrix<uint8_t> red_channel_filt = simple_filter.simple_filter_2_dims(red_channel, kernel);
+		Matrix<uint8_t> green_channel_filt = simple_filter.simple_filter_2_dims(green_channel, kernel);
+		Matrix<uint8_t> blue_channel_filt = simple_filter.simple_filter_2_dims(blue_channel, kernel);
+
+		red_channel = mat_tool.image_substract(red_channel, red_channel_filt);
+		green_channel = mat_tool.image_substract(green_channel, green_channel_filt);
+		blue_channel = mat_tool.image_substract(blue_channel, blue_channel_filt);
+
+		im_after_laplace_filter = split3channels2image(red_channel, green_channel, blue_channel);
+
+		//show result image
+		image_estimator get_ssim_psnr;
+		std::cout << "===============PSNR Estimating================= " << std::endl;
+
+		double psnr = get_ssim_psnr.getPSNR(blue_channe_one, input_image_two);
+		std::cout << "PSNR Reference_Res and Task_Res Image = " << psnr << std::endl;
+		psnr = get_ssim_psnr.getPSNR(blue_channe_one, im_after_gauss_noise);
+		std::cout << "Resized Image and After Gaussian Noise = " << psnr << std::endl;
+		psnr = get_ssim_psnr.getPSNR(blue_channe_one, im_after_gauss_filter);
+		std::cout << "Resized Image and After Gauss Filter = " << psnr << std::endl;
+		psnr = get_ssim_psnr.getPSNR(blue_channe_one, im_after_median_filter);
+		std::cout << "Resized Image and After Median Filter = " << psnr << std::endl;
+		psnr = get_ssim_psnr.getPSNR(blue_channe_one, im_after_nlm_filter);
+		std::cout << "Resized Image and After NLM Filter = " << psnr << std::endl;
+		psnr = get_ssim_psnr.getPSNR(blue_channe_one, im_after_laplace_filter);
+		std::cout << "Resized Image and After NLM Filter = " << psnr << std::endl;
+
+		std::cout << "===============SSIM Estimating================= " << std::endl;
+
+		cv::Scalar ssim = get_ssim_psnr.getMSSIM(blue_channe_one, input_image_two);
+		float ssim_aver = (ssim[0] + ssim[1] + ssim[2]) / 3;
+		std::cout << "PSNR Reference_Res and Task_Res Image = " << ssim_aver << std::endl;
+		ssim = get_ssim_psnr.getMSSIM(blue_channe_one, im_after_gauss_noise);
+		ssim_aver = (ssim[0] + ssim[1] + ssim[2]) / 3;
+		std::cout << "Resized Image and After Gaussian Noise = " << ssim_aver << std::endl;
+		ssim = get_ssim_psnr.getMSSIM(blue_channe_one, im_after_gauss_filter);
+		ssim_aver = (ssim[0] + ssim[1] + ssim[2]) / 3;
+		std::cout << "Resized Image and After Gauss Filter = " << ssim_aver << std::endl;
+		ssim = get_ssim_psnr.getMSSIM(blue_channe_one, im_after_median_filter);
+		ssim_aver = (ssim[0] + ssim[1] + ssim[2]) / 3;
+		std::cout << "Resized Image and After Median Filter = " << ssim_aver << std::endl;
+		ssim = get_ssim_psnr.getMSSIM(blue_channe_one, im_after_nlm_filter);
+		ssim_aver = (ssim[0] + ssim[1] + ssim[2]) / 3;
+		std::cout << "Resized Image and After NLM Filter = " << ssim_aver << std::endl;
+		ssim = get_ssim_psnr.getMSSIM(blue_channe_one, im_after_laplace_filter);
+		ssim_aver = (ssim[0] + ssim[1] + ssim[2]) / 3;
+		std::cout << "Resized Image and After NLM Filter = " << ssim_aver << std::endl;
+
+		std::cout << "=== SSIM Estimating and PSNR for Task_Res and Filtred Images ===" << std::endl;
+		psnr = get_ssim_psnr.getPSNR(blue_channe_one, im_after_laplace_filter);
+		std::cout << "PSNR Task_Res and Filtred Image = " << psnr << std::endl;
+		ssim = get_ssim_psnr.getMSSIM(blue_channe_one, im_after_laplace_filter);
+		ssim_aver = (ssim[0] + ssim[1] + ssim[2]) / 3;
+		std::cout << "SSIM Task_Res and Filtred Image = " << ssim_aver << std::endl;
+
+		imshow("Image Task", blue_channe_one);
+		imshow("Image Reference", input_image_two);
+		imshow("Noised Image", im_after_gauss_noise);
+		imshow("Image After Gauss Filter", im_after_gauss_filter);
+		imshow("Image After Median Filter Filter", im_after_median_filter);
+		imshow("Image After NLM Filter", im_after_nlm_filter);
+		imshow("Image After Laplace Filter", im_after_laplace_filter);
+
+		cv::waitKey(0);
 
 	return 0;
 }
